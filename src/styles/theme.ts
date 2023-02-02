84f7705acc45ec281e3f20e5bc280d34508c3223
soyRena <rennanfelipers@gmail.com>
2023-02-02 11:42:49 -0300

    feat: adding colors to the theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,14 @@
 const theme = {
-   colors: { background: '#e1e1e6', text: '#222222', primary: '#0070f3' }
+   uiColors: {
+      white: '#FFFFFF',
+      lightWhite: '#FDFDFD',
+      lightGrey: '#F5F5F5',
+      text: '#212121',
+      primary: '#F2B807',
+      secondary: '#F28F16',
+      third: '#F5DB13',
+      danger: '#D93E30'
+   }
 }
 
 export default theme
